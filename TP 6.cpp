547a9097c744babaaeb3cf356a0d020997f32cd6
lopezmateo <163901238+lopezmateo@users.noreply.github.com>
2024-04-13 10:31:45 -0300

    Add files via upload

---
+++ TP 6.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include<string>
+
+using namespace std;
+int main ()
+{
+	int n1,n2;
+	
+	
+	string cartel ="el mayor es el n2";
+//	cout<<cartel1<<endl;
+	
+	cout<<"ingrese un valor"<<endl;
+	cin>>n1;
+	cout<<"ingrese otro valor"<<endl;
+	cin>>n2;
+	
+	if(n1>n2 )
+	{
+		cartel="el mayor es el n1";
+    }
+		cout<<cartel<<endl;
+		
+		
+		return 0;
+
+}
+	
+	
+	
